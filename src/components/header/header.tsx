092b7c3d9f1cb27841e405766d2ea97668b9bb26
Laize Ferraz <laizeferraz@gmail.com>
2025-09-25 19:08:19 +1000

    create header component & bring back initial home style

---
+++ src/components/header/header.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link"
+import { ActiveLink } from "../active-link/active-link"
+import { Button } from "../ui/button"
+
+export function Header() {
+  return (
+    <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 ls:px-80">
+        <div className="flex h-16 items-center justify-between">
+          <Link href="/">Logo</Link>
+          <nav className="flex items-center gap-6">
+            <ActiveLink href="/">Home</ActiveLink>
+            <ActiveLink href="/blig">Blog</ActiveLink>
+            <Button variant="secondary" className="rounded-full">
+              <Link href="/start">Start</Link>
+            </Button>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/components/header/header";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -12,8 +13,9 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full bg-slate-900">
-      <h2 className="text-4xl text-gray-200">Hello world!</h2>
+    <div className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}>
+      <Header />
+      <h2>Hello</h2>
     </div>
   );
 }
