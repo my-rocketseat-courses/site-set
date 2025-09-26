b7a878ed75ffa1fa687601c9083510e6f9351f65
Laize Ferraz <laizeferraz@gmail.com>
2025-09-26 20:17:50 +1000

    create and apply layout

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,16 @@
+import { Header } from "../header/header";
+
+type LayoutProps = {
+  children: React.ReactNode
+}
+
+export function Layout({children}: LayoutProps) {
+  return (
+    <div className="relative flex min-h-screen flex-col bg-gray-700">
+      <Header />
+      <main className="flex-1 flex flex-col mb-12">
+        {children}
+      </main>
+    </div>
+  )
+}
