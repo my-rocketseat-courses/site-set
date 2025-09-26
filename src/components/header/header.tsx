9186ddb73e630208c81a200e4a6d81bb3cbe2fb2
Laize Ferraz <laizeferraz@gmail.com>
2025-09-26 20:03:53 +1000

    create logo component and add logo image

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link"
 import { ActiveLink } from "../active-link/active-link"
 import { Button } from "../ui/button"
+import { Logo } from "../logo/logo"
 
 export function Header() {
   return (
     <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 ls:px-80">
         <div className="flex h-16 items-center justify-between">
-          <Link href="/">Logo</Link>
+          <Logo />
           <nav className="flex items-center gap-6">
             <ActiveLink href="/">Home</ActiveLink>
             <ActiveLink href="/blig">Blog</ActiveLink>
---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export function Logo() {
+  return (
+    <Link href="/" title="Home page">
+      <Image
+        src="/logo.svg"
+        alt="Site Set logo"
+        width={116}
+        height={32}
+      />
+    </Link>
+  )
+}
